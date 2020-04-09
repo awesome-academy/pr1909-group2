9a08e7f5e4fd788038ae6d6ec9fa0050f30798c5
hvu8412 <hvu8412@gmail.com>
2020-04-09 10:47:23 +0700

    them truong name

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get "static_pages/home"
   root "static_pages#home"
+  # get '/users/sign_out' => 'devise/sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
