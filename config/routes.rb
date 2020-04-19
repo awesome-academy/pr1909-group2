2c8ceacb38a87a4d06d9620c4bdbc8cb856c8a6e
lvn17800 <lvn17800@gmail.com>
2020-04-19 11:25:54 +0700

    fix conflic

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     devise_for :users
     get "static_pages/home"
-    root "static_pages#home"
     resources :users, only: [:index, :edit]
-    get 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
     root "books#index"
+    get 'users/:id' => 'users#destroy', :via => :delete, :as => :admin_destroy_user
     resources :books
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
