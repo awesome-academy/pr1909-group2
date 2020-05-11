95cc9ae7458d7d5bb5ca289635158312a4b85eb1
lvn17800 <lvn17800@gmail.com>
2020-05-11 18:36:28 +0700

    Tao Tac Gia

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,25 @@
+class AuthorsController < ApplicationController
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      flash[:success] = "Create author success!"
+      redirect_to @author
+    else
+      render :new
+    end
+  end
+
+  def show
+    @author = Author.find_by id: params[:id]
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :age, :country)
+  end
+end
