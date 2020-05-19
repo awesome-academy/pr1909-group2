6d74112e6bebb2e08a74c03fef69057119f1c10b
lvn17800 <lvn17800@gmail.com>
2020-05-19 20:50:04 +0700

    fix bug

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,8 +2,8 @@ class Author < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :country, presence: true
   validates :age, presence: true
-  has_many :author_books
+  has_many :author_books, dependent: :destroy
   has_many :books, through: :author_books
-  has_many :author_publishers
+  has_many :author_publishers, dependent: :destroy
   has_many :publishers, through: :author_publishers
 end
