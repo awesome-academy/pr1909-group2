9b8ee1f8b1f923b8c66d22363d5e52c718bfff4d
lvn17800 <lvn17800@gmail.com>
2020-05-25 13:36:52 +0700

    update cart

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
     resources :line_items
     resources :carts
     resources :orders
+    get "orders_active", to: "orders#active"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
