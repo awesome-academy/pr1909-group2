6ade51015ff991e45302dde28b26010a0d3fe659
hvu8412 <hvu8412@gmail.com>
2020-06-12 13:31:19 +0700

    login with fb google

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
-	def self.new_with_session params, session
-		super.tap do |user|
-      if data = session["devise.facebook_data"] &&
-        session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
-      end
-    end
-  end
 
   def self.from_omniauth auth
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_143950) do
+ActiveRecord::Schema.define(version: 2020_06_11_120529) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_10_143950) do
     t.string "name"
     t.string "provider"
     t.string "uid"
+    t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
