73b8eb0b01a9419c3db63059c7f5f9f2ba28cc63
hvu8412 <hvu8412@gmail.com>
2020-06-24 18:18:41 +0700

    fix bug

--- db/migrate/20200623074901_remove_team_id_from_player.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTeamIdFromPlayer < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :players, :team_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_23_074901) do
+ActiveRecord::Schema.define(version: 2020_06_19_114354) do
 
   create_table "players", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2020_06_23_074901) do
     t.string "weaknesses"
     t.integer "age"
     t.string "nationality"
+    t.bigint "team_id", null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
   end
 
   create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
