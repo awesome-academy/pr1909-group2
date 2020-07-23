5e0426b1bd0f1991495422b7ef73e3f7e6acbdb7
lvn17800 <lvn17800@gmail.com>
2020-07-23 20:26:21 +0700

    fix nho

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
-    @match_waits = Match.wait.paginate(:page => params[:page], :per_page => 7).order(start_match: :ASC)
+    @match_waits = Match.all.select { |x| x["start_match"].strftime("%m/%d/%Y") == Date.today.strftime("%m/%d/%Y") }
     @tournaments = Tournament.all
     @top_players = Player.paginate(:page => params[:page], :per_page => 7).order(point: :DESC)
   end
